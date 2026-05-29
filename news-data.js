1f35b847014fa221423c5277fe25f3e13e052993
yunitechhk2025 <yunitechhk@gmail.com>
2026-05-29 22:28:50 +0800

    update news-data.js

--- news-data.js
+++ news-data.js
@@ -1,18 +1,1 @@
-const newsData = [
-    {
-        "id": "n1780063715430",
-        "date": "2026-05-29",
-        "pinned": false,
-        "image": "https://yunitechhk.com/images/news/news-1780063710987.jpeg",
-        "title": {
-            "zh-tw": "jumpstarter",
-            "zh-cn": "jumpstarter",
-            "en": "jumpstarter"
-        },
-        "content": {
-            "zh-tw": "",
-            "zh-cn": "",
-            "en": ""
-        }
-    }
-];
+const newsData = [];
