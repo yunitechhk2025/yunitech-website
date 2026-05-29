e4332599a8b5d6d57945376d3312fc00c4621b94
yunitechhk2025 <yunitechhk@gmail.com>
2026-05-29 22:32:08 +0800

    update news-data.js

--- news-data.js
+++ news-data.js
@@ -1,1 +1,34 @@
-const newsData = [];
+const newsData = [
+    {
+        "id": "n1780065128108",
+        "date": "2026-04-28",
+        "pinned": false,
+        "image": "https://yunitechhk.com/images/news/news-1780065123744.jpeg",
+        "title": {
+            "zh-tw": "InnoEX 2026",
+            "zh-cn": "InnoEX 2026",
+            "en": "InnoEX 2026"
+        },
+        "content": {
+            "zh-tw": "参加了 2026 年 InnoEX 的 B4B 挑战赛",
+            "zh-cn": "参加了 2026 年 InnoEX 的 B4B 挑战赛",
+            "en": "参加了 2026 年 InnoEX 的 B4B 挑战赛"
+        }
+    },
+    {
+        "id": "n1780063715430",
+        "date": "2026-05-29",
+        "pinned": true,
+        "image": "https://yunitechhk.com/images/news/news-1780063710987.jpeg",
+        "title": {
+            "zh-tw": "jumpstarter",
+            "zh-cn": "jumpstarter",
+            "en": "jumpstarter"
+        },
+        "content": {
+            "zh-tw": "",
+            "zh-cn": "",
+            "en": ""
+        }
+    }
+];
